e349aff4afe8ea25b55be6a0302666fcfeef2b9e
carlossilvestri <carlossilvestri9@gmail.com>
2023-03-23 22:03:21 -0400

    Fix colors

--- js/src/custom.js
+++ js/src/custom.js
@@ -31,7 +31,16 @@ if (company) {
         case "blue":
             useBlue();
             break;
+        case "blue-d":
+            useDarkBlue();
+            break;
+        case "blue-o":
+            useBlue();
+            break;
         case "orange":
+            useOrange();
+            break;
+        case "orange-j":
             useOrange();
             break;
         case "yellow":
@@ -58,7 +67,9 @@ function useBlue() {
     $("body").get(0).style.setProperty("--primary-filter", "invert(21%) sepia(98%) saturate(2379%) hue-rotate(257deg) brightness(112%) contrast(101%)");
     $("body").get(0).style.setProperty("--secondary-filter", "invert(7%) sepia(28%) saturate(1007%) hue-rotate(197deg) brightness(97%) contrast(99%)");
 }
-
+function useDarkBlue() {
+    $("body").get(0).style.setProperty("--primary-color", "#000080");
+}
 function useRed() {
     $("body").get(0).style.setProperty("--primary-color", "#ff3c3c");
     $("body").get(0).style.setProperty("--primary-gradient", "linear-gradient(-20deg,var(--default-color-invert),var(--primary-color) 99%)");
@@ -72,7 +83,7 @@ function usePurple() {
 }
 
 function useOrange() {
-    $("body").get(0).style.setProperty("--primary-color", "#FF5733");
+    $("body").get(0).style.setProperty("--primary-color", "#ff8000");
 }
 function useGray() {
     $("body").get(0).style.setProperty("--primary-color", "#808080");
